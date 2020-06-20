19a6e07190bb3852ac07792142a5e1eb80937c33
Karl <karlrez123@gmail.com>
2020-06-19 21:39:36 -0700

    Updates

---
+++ precache-manifest.369f15f85bea9c7c2c4382b3c2076173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9bcbbdd9c33c96689eb59333dfb405",
+    "url": "/React-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "093e12faa03f98075826",
+    "url": "/React-tic-tac-toe/static/css/main.96b170b3.chunk.css"
+  },
+  {
+    "revision": "ba0c43db5c561089a580",
+    "url": "/React-tic-tac-toe/static/js/2.18e4a0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-tic-tac-toe/static/js/2.18e4a0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093e12faa03f98075826",
+    "url": "/React-tic-tac-toe/static/js/main.06000e3f.chunk.js"
+  },
+  {
+    "revision": "5009a2993f8c32138ed5",
+    "url": "/React-tic-tac-toe/static/js/runtime-main.d0a21d71.js"
+  }
+]);
